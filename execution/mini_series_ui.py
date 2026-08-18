3d4b5ff4aa4d7943c9b9b40ec14b450147c61781
Tylarkin <virallensemediavlm@gmail.com>
2026-08-19 00:51:58 +0700

    Bring Mini-Series & Multi-Scene Storyboard generator up to date with Seedance 2.5, 4K resolution, and up to 30s clips

--- execution/mini_series_ui.py
+++ execution/mini_series_ui.py
@@ -970,6 +970,8 @@ def mini_series_ui(user_asset_path, outfits_data, vibes_data, assets, knowledge_
                             v_engine = st.selectbox(
                                 "Video Engine", 
                                 [
+                                    "Seedance 2.5 (Reference-to-Video - Up to 50 Refs)",
+                                    "Seedance 2.5 (Image-to-Video)",
                                     "Seedance 2.0 (Reference-to-Video)",
                                     "Seedance 2.0 Mini (Reference-to-Video)",
                                     "Seedance 2.0 (Image-to-Video)",
@@ -980,11 +982,12 @@ def mini_series_ui(user_asset_path, outfits_data, vibes_data, assets, knowledge_
                             )
                             c_vres, c_var, c_vdur = st.columns(3)
                             with c_vres:
-                                v_res = st.selectbox("Resolution", ["1080P (Full HD)", "720P (Draft)"], index=0, key=f"v_res_{key_base}")
+                                v_res = st.selectbox("Resolution", ["1080P", "720P", "4K Ultra HD"], index=0, key=f"v_res_{key_base}")
                             with c_var:
                                 v_ar = st.selectbox("Aspect Ratio", ["16:9 (Widescreen)", "9:16 (Vertical / Reels)", "1:1 (Square)"], index=0, key=f"v_ar_{key_base}")
                             with c_vdur:
-                                v_dur = st.slider("Duration (Sec)", 2, 15, 5, key=f"v_dur_{key_base}")
+                                max_dur_val = 30 if "2.5" in v_engine else 15
+                                v_dur = st.slider("Duration (Sec)", 2, max_dur_val, min(5, max_dur_val), key=f"v_dur_{key_base}")
                             
                             all_cast_keys = list(st.session_state.cast_lookup_map.keys())
                             
@@ -1185,7 +1188,11 @@ def mini_series_ui(user_asset_path, outfits_data, vibes_data, assets, knowledge_
                                             with open(temp_a_path, "wb") as f_a:
                                                 f_a.write(up_aref.getbuffer())
 
-                                        if "Mini" in v_engine:
+                                        if "2.5" in v_engine and "Reference" in v_engine:
+                                            target_model = "bytedance/seedance-2.5/reference-to-video"
+                                        elif "2.5" in v_engine and "Image" in v_engine:
+                                            target_model = "bytedance/seedance-2.5/image-to-video"
+                                        elif "Mini" in v_engine:
                                             target_model = "bytedance/seedance-2.0-mini/reference-to-video"
                                         elif "Seedance" in v_engine and "Reference" in v_engine:
                                             target_model = "bytedance/seedance-2.0/reference-to-video"
@@ -1194,7 +1201,7 @@ def mini_series_ui(user_asset_path, outfits_data, vibes_data, assets, knowledge_
                                         elif "Wan" in v_engine:
                                             target_model = "alibaba/wan-2.7/image-to-video"
                                         else:
-                                            target_model = "bytedance/seedance-2.0/reference-to-video"
+                                            target_model = "bytedance/seedance-2.5/reference-to-video"
                                             
                                         from execution.generate_wan import generate_wan_video
                                         res_video = generate_wan_video(
